5565f4a0a5ce2fe2603319b7a5f46e82e6e34afa
NetworkCats <networkcatlol@gmail.com>
2026-03-08 05:52:42 +0800

    feat: add HTTP port 80 support with Cloudflare-only firewall rules

--- deploy/terraform/firewall.tf
+++ deploy/terraform/firewall.tf
@@ -13,6 +13,18 @@ resource "vultr_firewall_rule" "ssh_v4" {
   notes             = "Allow SSH from anywhere"
 }
 
+# Allow HTTP from Cloudflare only (IPv4)
+resource "vultr_firewall_rule" "http_cloudflare_v4" {
+  firewall_group_id = vultr_firewall_group.rustyip.id
+  protocol          = "tcp"
+  ip_type           = "v4"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  port              = "80"
+  source            = "cloudflare"
+  notes             = "Allow HTTP from Cloudflare IPv4"
+}
+
 # Allow HTTPS from Cloudflare only (IPv4)
 resource "vultr_firewall_rule" "https_cloudflare_v4" {
   firewall_group_id = vultr_firewall_group.rustyip.id
